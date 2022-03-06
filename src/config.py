b6bb9f2c4ea3323aa5f0d6ad26cef321573b6334
m-alorda <miguel.alorda.cantero@gmail.com>
2022-03-06 18:50:42 +0100

    Added pdf generation and bootstrap styles

--- src/config.py
+++ src/config.py
@@ -7,8 +7,8 @@ PROJECT_DIR = Path(__file__).resolve().parent.parent
 _CONFIG_FILE = PROJECT_DIR / "config/config.yaml"
 _SECRET_CONFIG_FILE = PROJECT_DIR / "secrets/config.yaml"
 
-with _SECRET_CONFIG_FILE.open() as f:
+with _SECRET_CONFIG_FILE.open(encoding="utf-8") as f:
     secret_config = yaml.safe_load(f)
 
-with _CONFIG_FILE.open() as f:
+with _CONFIG_FILE.open(encoding="utf-8") as f:
     config = yaml.safe_load(f)
--- src/report.py
+++ src/report.py
@@ -1,9 +1,11 @@
 import dataclasses
 import datetime
+from pathlib import Path
 from typing import Collection, Iterable, Iterator
 
 import jinja2
 import pipe
+import xhtml2pdf.pisa
 
 import config
 import model
@@ -53,13 +55,28 @@ def _generate_rendered_report_lines(data: ReportData) -> Iterator[str]:
     yield from template.generate(data=data)
 
 
-def generate_report(
+def generate_html_report(
     events_dict: dict[model.EventType, Iterable[model.CalendarEventMetaData]],
-) -> None:
+) -> Path:
     data = _create_report_data(events_dict)
     dest_dir = config.PROJECT_DIR / "reports"
-    dest_dir.mkdir()
+    dest_dir.mkdir(exist_ok=True)
     dest_file = dest_dir / f"report-{data.current_date}.html"
-    with dest_file.open("w") as f:
+    with dest_file.open("w", encoding="utf-8") as f:
         for line in _generate_rendered_report_lines(data):
             f.write(line)
+    return dest_file
+
+
+def generate_report(
+    events_dict: dict[model.EventType, Iterable[model.CalendarEventMetaData]],
+) -> Path:
+    tmp_html_report_path = generate_html_report(events_dict)
+    dest_file_path = tmp_html_report_path.with_suffix(".pdf")
+    with (
+        tmp_html_report_path.open(encoding="utf-8") as src_file,
+        dest_file_path.open("wb") as dest_file,
+    ):
+        xhtml2pdf.pisa.CreatePDF(src_file, dest_file)
+    # tmp_html_report_path.unlink()
+    return dest_file_path
